0165e90e36ed62f4c84ad7b0f45fd5c30e569a8c
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-08 19:31:26 +0700

    wip

--- app/sink/login-form.tsx
+++ app/sink/login-form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { CardLoginFormDemo } from '@/components/docs/surfaces/card-login-form-demo'
+import CardLoginFormDemo from '@/components/docs/surfaces/card-login-form-demo'
 
 export function LoginForm() {
   return <CardLoginFormDemo />
