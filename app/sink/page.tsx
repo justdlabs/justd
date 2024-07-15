ffc51ca0ade60d512c582e5e01daec5983427e88
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-15 14:19:57 +0700

    complete the docs for visally hidden components

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,11 +1,7 @@
-import ModalDemo from '@/components/docs/overlays/modal-demo'
-import { VisuallyHidden } from 'ui'
-
 export default function Page() {
   return (
     <div className="p-32">
-      <ModalDemo />
-      <VisuallyHidden>This is a hidden element</VisuallyHidden>
+      <div className="max-w-2xl mx-auto"></div>
     </div>
   )
 }
