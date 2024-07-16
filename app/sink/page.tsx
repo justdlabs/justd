247f7f15d9ab06b211bcab3281a2b0fa9b647048
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-16 13:26:33 +0700

    fix toaster

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import TooltipDemo from '@/components/docs/overlays/tooltip-demo'
+import ToasterPositionsDemo from '@/components/docs/statuses/toaster-positions-demo'
 import GridGapDemo from '@/components/docs/surfaces/grid-gap-demo'
-import { Container } from 'ui'
+import { Container, Toaster } from 'ui'
 
 export default function Page() {
   return (
     <div className="py-6">
       <Container>
-        <GridGapDemo />
+        <Toaster />
+        <ToasterPositionsDemo />
       </Container>
     </div>
   )
