da61559e490e354a26e16f4cf30b1ef90d88729e
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-16 13:35:28 +0700

    wip

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import TooltipDemo from '@/components/docs/overlays/tooltip-demo'
 import ToasterPositionsDemo from '@/components/docs/statuses/toaster-positions-demo'
-import GridGapDemo from '@/components/docs/surfaces/grid-gap-demo'
 import { Container, Toaster } from 'ui'
 
 export default function Page() {
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -78,7 +78,7 @@ const buttonStyles = tv(
         medium:
           'h-10 px-[calc(theme(spacing.4)-1px)] py-[calc(theme(spacing.2)-1px)] text-base lg:text-sm/6',
         large:
-          'h-11 px-[calc(theme(spacing.5)-1px)] py-[calc(theme(spacing[2.5])-1px)] text-base/7 lg:text-base/7 [&>[data-slot=icon]]:size-5',
+          'h-10 sm:h-11 px-[calc(theme(spacing.4)-1px)] sm:px-[calc(theme(spacing.5)-1px)] py-[calc(theme(spacing[2.5])-1px)] text-base lg:text-base/7 sm:[&>[data-slot=icon]]:size-5',
         'square-petite': 'size-9'
       },
       shape: {
