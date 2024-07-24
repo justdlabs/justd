df73e59ae3ca228aad9c5d8679d060244431d4f5
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-24 15:04:34 +0700

    make grid docs more clear

--- app/(app)/colors/color-palette.tsx
+++ app/(app)/colors/color-palette.tsx
@@ -16,7 +16,7 @@ import { IconBrandTailwindcss, IconCheck, IconDuplicate } from '@irsyadadl/paran
 import { parseColor } from '@react-stately/color'
 import type { ColorFormat } from '@react-types/color'
 import { AnimatePresence, motion } from 'framer-motion'
-import type { ListBoxItemProps } from 'react-aria-components'
+import type { GridListItemProps } from 'react-aria-components'
 import { GridListItem, Text, ToggleButton } from 'react-aria-components'
 import { useInView } from 'react-intersection-observer'
 import { toast } from 'sonner'
@@ -63,7 +63,7 @@ export function ColorPalette() {
         setHasMore(false)
       }
     }
-  }, [colors.length, hasMore, inView])
+  }, [inView])
   return (
     <>
       <HeaderPrimitive className="bg-background pb-4 pt-12 lg:py-16 border-b ">
@@ -99,7 +99,7 @@ export function ColorPalette() {
   )
 }
 
-interface ColorPaletteProps extends ListBoxItemProps {
+interface ColorPaletteProps {
   item: ColorItem
 }
 
@@ -160,32 +160,33 @@ export function ColorRow({ item }: ColorPaletteProps) {
           gapY={{ initial: 3, sm: 1 }}
           gapX={{ initial: 1, sm: 1 }}
           aria-label={`${item.name} 50-950 colors`}
+          items={item.children}
         >
-          {item.children.map(({ shade, color }, i) => (
+          {(color) => (
             <ColorItem
-              key={i}
+              id={Math.random().toString()}
               {...{
                 isForTailwindVariable,
                 selectedFormat: selectedFormat ?? 'hsl',
-                item: { shade, color },
+                item: color,
                 name: item.name
               }}
             />
-          ))}
+          )}
         </Grid>
       </div>
     </>
   )
 }
 
-interface GridListItem {
+interface ColorItemProps extends GridListItemProps {
   item: ColorShade
   name: string
   isForTailwindVariable: boolean
   selectedFormat: FormatOnlyForTailwindVariableType | ColorFormat
 }
 
-const ColorItem = ({ item, name, isForTailwindVariable, selectedFormat }: GridListItem) => {
+const ColorItem = ({ item, name, isForTailwindVariable, selectedFormat }: ColorItemProps) => {
   const [copied, setCopied] = React.useState(false)
   const handleCopy = async (selectedColor: string) => {
     const toCopy = isForTailwindVariable
