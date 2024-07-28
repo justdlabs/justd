81f6bad2d02f2a418bf65b79f14064c1afc583d8
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-28 16:22:23 +0700

    wip

--- app/(app)/sink/page.tsx
+++ app/(app)/sink/page.tsx
@@ -1,18 +1,7 @@
 'use client'
 
-import GridListDragDemo from '@/components/docs/collections/grid-list-drag-demo'
-import TableBulkDemo from '@/components/docs/collections/table-bulk-demo'
-import TableDemo from '@/components/docs/collections/table-demo'
-import TableDragDemo from '@/components/docs/collections/table-drag-demo'
-import TableSortingDemo from '@/components/docs/collections/table-sorting-demo'
-import { Container, Grid } from 'ui'
-
 export default function Page() {
   return (
-    <Container className="py-24 mx-auto">
-      <Grid columns={1} gap={10}>
-        <TableSortingDemo />
-      </Grid>
-    </Container>
+    <div/>
   )
 }
