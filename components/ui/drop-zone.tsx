7120587af33082bc2bf4f6e8f1638f2c8d93c810
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-29 15:31:08 +0700

    add more examples to fileTrigger and DropZones

--- components/ui/drop-zone.tsx
+++ components/ui/drop-zone.tsx
@@ -1,19 +1,26 @@
 'use client'
 
-import { DropZone as DropPrimitiveZone, type DropZoneProps } from 'react-aria-components'
+import React from 'react'
 
-import { cn } from './primitive'
+import { composeRenderProps, DropZone as DropPrimitiveZone, type DropZoneProps } from 'react-aria-components'
+import { tv } from 'tailwind-variants'
+
+import { focusStyles } from './primitive'
+
+const dropZoneStyles = tv({
+  extend: focusStyles,
+  base: 'group has-[slot=description]:text-center flex max-h-[200px] p-6 max-w-xl flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm',
+  variants: {
+    isDropTarget: {
+      true: 'bg-primary/10 ring-4 ring-primary/20 [&_.text-muted-fg]:text-primary-fg border-solid border-primary'
+    }
+  }
+})
 
 const DropZone = ({ className, ...props }: DropZoneProps) => (
   <DropPrimitiveZone
-    className={(values) =>
-      cn(
-        'flex h-[150px] w-[300px] flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm data-[drop-target]:border-solid data-[drop-target]:border-primary data-[drop-target]:bg-accent',
-        typeof className === 'function' ? className(values) : className
-      )
-    }
+    className={composeRenderProps(className, (className, renderProps) => dropZoneStyles({ ...renderProps, className }))}
     {...props}
   />
 )
-
 export { DropZone }
