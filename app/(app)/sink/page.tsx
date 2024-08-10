16fb6107f2ed51401ca38f9161367b7ce5598385
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-10 21:14:09 +0700

    ff

--- app/(app)/sink/page.tsx
+++ app/(app)/sink/page.tsx
@@ -2,6 +2,15 @@
 
 import React from 'react'
 
+import ColorPickerCombinationDemo from '@/components/docs/colors/color-picker-combination-demo'
+import ColorPickerDemo from '@/components/docs/colors/color-picker-demo'
+
 export default function Page() {
-  return <div />
+  return (
+    <div>
+      <div className="max-w-xs">
+        <ColorPickerCombinationDemo />
+      </div>
+    </div>
+  )
 }
