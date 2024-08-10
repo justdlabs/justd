ec7f5fa882043a2cb5d6e831c44b2f8208a75afe
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-10 20:44:28 +0700

    ff

--- app/(app)/sink/page.tsx
+++ app/(app)/sink/page.tsx
@@ -2,14 +2,6 @@
 
 import React from 'react'
 
-import TagFieldIntentDemo from '@/components/docs/forms/tag-field-intent-demo'
-import TagFieldShapeDemo from '@/components/docs/forms/tag-field-shape-demo'
-
 export default function Page() {
-  return (
-    <div>
-      <TagFieldIntentDemo />
-      <TagFieldShapeDemo />
-    </div>
-  )
+  return <div />
 }
