19dc75720bd4ed8ca3684c701445d97b9b3b9f9e
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-13 17:21:16 +0700

    add missing loader at toast

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 
 import { useTheme } from '@/components/theme-provider'
-import { Loader } from '@/components/ui/loader'
+import { Loader } from './loader'
 import { IconCheck, IconCircleInfoFill, IconTriangleInfoFill } from 'justd-icons'
 import { Toaster as ToasterPrimitive, type ToasterProps } from 'sonner'
 import { twJoin } from 'tailwind-merge'
