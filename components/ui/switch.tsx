418c8a7868f7f935cb7cd2f3060d4e7f98e2a006
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-14 23:53:54 +0700

    add meta to slug

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,6 +1,9 @@
 'use client'
 
-import { Switch as SwitchPrimitive, type SwitchProps as SwitchPrimitiveProps } from 'react-aria-components'
+import {
+  Switch as SwitchPrimitive,
+  type SwitchProps as SwitchPrimitiveProps
+} from 'react-aria-components'
 import { tv, type VariantProps } from 'tailwind-variants'
 
 import { focusRing } from './primitive'
