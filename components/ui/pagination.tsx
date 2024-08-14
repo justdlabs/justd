4a4b2e6357b110e54ec7d7c5e838e82a83177d00
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-08-14 22:28:41 +0700

    fix pagination lint

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -17,10 +17,10 @@ import {
   SectionProps,
   Separator
 } from 'react-aria-components'
+import { tv } from 'tailwind-variants'
 
 import { buttonStyles } from './button'
 import { cn } from './primitive'
-import { tv } from 'tailwind-variants'
 
 const paginationStyles = tv({
   slots: {
@@ -86,7 +86,16 @@ const renderListItem = (
 interface PaginationItemProps extends ListBoxItemProps {
   children?: React.ReactNode
   className?: string
-  intent?: 'primary' | 'secondary' | 'danger' | 'warning' | 'info' | 'light/dark' | 'success' | 'light' | 'dark'
+  intent?:
+    | 'primary'
+    | 'secondary'
+    | 'danger'
+    | 'warning'
+    | 'info'
+    | 'light/dark'
+    | 'success'
+    | 'light'
+    | 'dark'
   size?: 'medium' | 'large' | 'square-petite' | 'extra-small' | 'small'
   shape?: 'square' | 'circle'
   appearance?: 'solid' | 'outline' | 'plain'
@@ -106,7 +115,11 @@ const PaginationItem = ({
   ...props
 }: PaginationItemProps) => {
   const textValue =
-    typeof children === 'string' ? children : typeof children === 'number' ? children.toString() : undefined
+    typeof children === 'string'
+      ? children
+      : typeof children === 'number'
+        ? children.toString()
+        : undefined
 
   const renderPaginationIndicator = (indicator: React.ReactNode) =>
     renderListItem(
