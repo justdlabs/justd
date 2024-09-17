25a9089261c02975cf1fcf89ecc42de0399822d3
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-09-17 18:21:43 +0700

    fix toast

--- app/(app)/docs/[...slug]/page.tsx
+++ app/(app)/docs/[...slug]/page.tsx
@@ -83,7 +83,6 @@ export default async function PostPage({ params }: DocPageProps) {
 
   return (
     <>
-      {doc.title === "Toast" && <Toast />}
       <div className="min-w-0 max-w-2xl flex-auto pt-16 pb-32 lg:max-w-none px-4 lg:pl-8 lg:pr-0 xl:px-16">
         <main className="prose prose-img:rounded-lg prose-pre:p-0 prose-headings:mb-[0.3rem] prose-headings:scroll-mt-24 prose-blue dark:prose-invert max-w-[inherit]">
           <h1 className="mb-2 font-semibold tracking-tight sm:tracking-tight text-2xl sm:text-3xl">
