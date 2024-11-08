e18bf7d4be191a2d281b47f437470f2d3b5cf1f3
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-11-09 05:28:10 +0700

    lfg

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,45 +15,45 @@ const buttonStyles = tv(
     extend: focusButtonStyles,
     base: [
       "kbt32x before:absolute after:absolute box-border relative no-underline isolate inline-flex items-center justify-center gap-x-2 border font-medium",
-      "forced-colors:[--btn-icon:ButtonText] forced-colors:hover:[--btn-icon:ButtonText]",
-      "[&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-1 [&>[data-slot=icon]]:size-4 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon]"
+      "forced-colors:[--button-icon:ButtonText] forced-colors:hover:[--button-icon:ButtonText]",
+      "[&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-1 [&>[data-slot=icon]]:size-4 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--button-icon]"
     ],
     variants: {
       intent: {
         primary: [
-          "text-primary-fg [--btn-bg:theme(colors.primary.DEFAULT)] [--btn-border:theme(colors.primary.DEFAULT)] [--btn-hover-overlay:theme(colors.white/10%)]",
-          "[--btn-icon:theme(colors.primary.fg/60%)] active:[--btn-icon:theme(colors.primary.fg/80%)] hover:[--btn-icon:theme(colors.primary.fg/80%)]"
+          "text-primary-fg [--button-bg:theme(colors.primary.DEFAULT)] [--button-border:theme(colors.primary.DEFAULT)] [--button-hover-overlay:theme(colors.white/10%)]",
+          "[--button-icon:theme(colors.primary.fg/60%)] active:[--button-icon:theme(colors.primary.fg/80%)] hover:[--button-icon:theme(colors.primary.fg/80%)]"
         ],
         secondary: [
-          "text-secondary-fg [--btn-bg:theme(colors.secondary.DEFAULT)] dark:[--btn-bg:theme(colors.secondary.DEFAULT)] [--btn-border:theme(colors.secondary.fg/10%)] [--btn-hover-overlay:theme(colors.secondary.fg/2.5%)] data-[active]:[--btn-border:theme(colors.secondary.fg/15%)] hover:[--btn-border:theme(colors.secondary.fg/15%)]",
-          "[--btn-icon:theme(colors.muted.fg)] active:[--btn-icon:theme(colors.fg)] hover:[--btn-icon:theme(colors.fg)]"
+          "text-secondary-fg [--button-bg:theme(colors.secondary.DEFAULT)] dark:[--button-bg:theme(colors.secondary.DEFAULT)] [--button-border:theme(colors.secondary.fg/10%)] [--button-hover-overlay:theme(colors.secondary.fg/2.5%)] data-[active]:[--button-border:theme(colors.secondary.fg/15%)] hover:[--button-border:theme(colors.secondary.fg/15%)]",
+          "[--button-icon:theme(colors.muted.fg)] active:[--button-icon:theme(colors.fg)] hover:[--button-icon:theme(colors.fg)]"
         ],
         warning: [
-          "text-warning-fg [--btn-bg:theme(colors.warning.DEFAULT)] [--btn-border:theme(colors.warning.DEFAULT)] [--btn-hover-overlay:theme(colors.white/10%)]",
-          "[--btn-icon:theme(colors.warning.fg/60%)] active:[--btn-icon:theme(colors.warning.fg/80%)] hover:[--btn-icon:theme(colors.warning.fg/80%)]"
+          "text-warning-fg [--button-bg:theme(colors.warning.DEFAULT)] [--button-border:theme(colors.warning.DEFAULT)] [--button-hover-overlay:theme(colors.white/10%)]",
+          "[--button-icon:theme(colors.warning.fg/60%)] active:[--button-icon:theme(colors.warning.fg/80%)] hover:[--button-icon:theme(colors.warning.fg/80%)]"
         ],
         danger: [
-          "text-white [--btn-bg:theme(colors.danger.DEFAULT)] [--btn-border:theme(colors.danger.DEFAULT)] [--btn-hover-overlay:theme(colors.white/10%)]",
-          "[--btn-icon:theme(colors.white/60%)] active:[--btn-icon:theme(colors.white/80%)] hover:[--btn-icon:theme(colors.white/80%)]"
+          "text-white [--button-bg:theme(colors.danger.DEFAULT)] [--button-border:theme(colors.danger.DEFAULT)] [--button-hover-overlay:theme(colors.white/10%)]",
+          "[--button-icon:theme(colors.white/60%)] active:[--button-icon:theme(colors.white/80%)] hover:[--button-icon:theme(colors.white/80%)]"
         ]
       },
       appearance: {
         solid: [
-          "border-transparent bg-[--btn-border]",
-          "before:inset-0 before:-z-10 before:bg-[--btn-bg] before:shadow before:data-[disabled]:shadow-none",
-          "after:shadow-[shadow:inset_0_1px_theme(colors.white/15%)] after:active:bg-[--btn-hover-overlay] after:hover:bg-[--btn-hover-overlay] after:data-[disabled]:shadow-none after:inset-0 after:-z-10",
-          "dark:after:-inset-px dark:before:hidden dark:border-white/5 dark:bg-[--btn-bg]"
+          "border-transparent bg-[--button-border]",
+          "before:inset-0 before:-z-10 before:bg-[--button-bg] before:shadow before:data-[disabled]:shadow-none",
+          "after:shadow-[shadow:inset_0_1px_theme(colors.white/15%)] after:active:bg-[--button-hover-overlay] after:hover:bg-[--button-hover-overlay] after:data-[disabled]:shadow-none after:inset-0 after:-z-10",
+          "dark:after:-inset-px dark:before:hidden dark:border-white/5 dark:bg-[--button-bg]"
         ],
         outline: [
-          "border-border text-fg [--btn-icon:theme(colors.muted.fg)]",
-          "hover:[--btn-icon:theme(colors.fg)] hover:bg-secondary/90",
-          "active:bg-secondary/90 active:[--btn-icon:theme(colors.fg)]"
+          "border-border text-fg [--button-icon:theme(colors.muted.fg)]",
+          "hover:[--button-icon:theme(colors.fg)] hover:bg-secondary/90",
+          "active:bg-secondary/90 active:[--button-icon:theme(colors.fg)]"
         ],
         plain: [
-          "border-transparent text-fg [--btn-icon:theme(colors.muted.fg)]",
+          "border-transparent text-fg [--button-icon:theme(colors.muted.fg)]",
           "pressed:bg-secondary/90",
-          "active:[--btn-icon:theme(colors.fg)] active:bg-secondary/90",
-          "hover:[--btn-icon:theme(colors.fg)] hover:bg-secondary/90"
+          "active:[--button-icon:theme(colors.fg)] active:bg-secondary/90",
+          "hover:[--button-icon:theme(colors.fg)] hover:bg-secondary/90"
         ]
       },
       size: {
