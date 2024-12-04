76d88248772676d6973d6d7ee06198e2e2add123
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-12-04 19:24:27 +0700

    wip

--- components/ui/note.tsx
+++ components/ui/note.tsx
@@ -14,12 +14,12 @@ const noteStyles = tv({
         "dark:[&_a]:text-secondary-fg dark:**:data-[slot=icon]:text-secondary-fg"
       ],
       info: [
-        "bg-sky-500/15 text-sky-700 group-data-[hover]:bg-sky-500/25 dark:bg-sky-500/10 dark:text-sky-300 dark:group-data-[hover]:bg-sky-500/20"
+        "bg-sky-500/15 text-sky-700 group-data-hovered:bg-sky-500/25 dark:bg-sky-500/10 dark:text-sky-300 dark:group-data-hovered:bg-sky-500/20"
       ],
       warning:
-        "bg-amber-400/20 text-amber-700 group-data-[hover]:bg-amber-400/30 dark:bg-amber-400/10 dark:text-amber-400 dark:group-data-[hover]:bg-amber-400/15",
+        "bg-amber-400/20 text-amber-700 group-data-hovered:bg-amber-400/30 dark:bg-amber-400/10 dark:text-amber-400 dark:group-data-hovered:bg-amber-400/15",
       danger:
-        "bg-red-500/15 text-red-700 group-data-[hover]:bg-red-500/25 dark:bg-red-500/10 dark:text-red-400 dark:group-data-[hover]:bg-red-500/20",
+        "bg-red-500/15 text-red-700 group-data-hovered:bg-red-500/25 dark:bg-red-500/10 dark:text-red-400 dark:group-data-hovered:bg-red-500/20",
       success: [
         "border-emerald-500/20 [&_a]:text-emerald-600 text-emerald-900 bg-emerald-50/50 **:data-[slot=icon]:text-emerald-600 leading-4",
         "dark:bg-emerald-500/10 dark:text-emerald-200 dark:[&_a]:text-emerald-50 dark:**:data-[slot=icon]:text-emerald-400"
