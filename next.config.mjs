cbfd1981fd117bcf75c5ce3efeb8aac142682e80
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-12-17 11:23:16 +0700

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -11,5 +11,29 @@ if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
 export default {
     experimental: {
         optimizePackageImports: ["shiki"]
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: "/_next/static/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+                ],
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/docs/1.x/:path*",
+                destination: "https://1x.getjustd.com/docs/1.x/:path*",
+            },
+            {
+                source: "/_next/static/:path*",
+                destination: "https://1x.getjustd.com/_next/static/:path*",
+            },
+        ];
+    },
 }
