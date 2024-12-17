b14bef0f00380c07746375c93f7d4bcfd90f43d7
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-12-17 10:29:16 +0100

    Add Asset Prefix

--- next.config.mjs
+++ next.config.mjs
@@ -8,87 +8,8 @@ if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
 
 /** @type {import('next').NextConfig} */
 export default {
+	assetPrefix: "/docs/1.x",
 	experimental: {
 		optimizePackageImports: ["shiki"],
-	},
-	assetPrefix: "/docs/1.x",
-	async redirects() {
-		return [
-			{
-				source: "/d/:path*",
-				destination: "/:path*",
-				permanent: true,
-			},
-			{
-				source: "/docs",
-				destination: "/docs/getting-started/installation",
-				permanent: false,
-			},
-			{
-				source: "/docs/components/layouts/aside",
-				destination: "/docs/components/layouts/sidebar",
-				permanent: false,
-			},
-			{
-				source: "/d/accordion",
-				destination: "/docs/components/navigation/disclosure-group",
-				permanent: false,
-			},
-			{
-				source: "/accordion",
-				destination: "/docs/components/navigation/disclosure-group",
-				permanent: false,
-			},
-			{
-				source: "/aside",
-				destination: "/docs/components/layouts/sidebar",
-				permanent: false,
-			},
-			{
-				source: "/design-system",
-				destination: "/docs/getting-started/installation",
-				permanent: false,
-			},
-			{
-				source: "/docs/components/collections/accordion",
-				destination: "/docs/components/navigation/disclosure-group",
-				permanent: true,
-			},
-			{
-				source: "/docs/components/controls/command",
-				destination: "/docs/components/controls/command-menu",
-				permanent: false,
-			},
-			{
-				source: "/docs/components/forms/select",
-				destination: "/docs/components/pickers/select",
-				permanent: false,
-			},
-			{
-				source: "/docs/components/forms/select",
-				destination: "/docs/components/pickers/select",
-				permanent: false,
-			},
-			{
-				source: "/docs/components/statuses/toaster",
-				destination: "/docs/components/statuses/toast",
-				permanent: true,
-			},
-			{
-				source: "/docs/components/collections/carousel",
-				destination: "/docs/components/media/carousel",
-				permanent: true,
-			},
-			{
-				source: "/docs/components/forms/choicebox",
-				destination: "/docs/components/collections/choicebox",
-				permanent: true,
-			},
-			{
-				source: "/docs/components/forms/multiple-select",
-				destination: "/docs/components/pickers/multiple-select",
-				permanent: false,
-			},
-		];
 	},
 };
